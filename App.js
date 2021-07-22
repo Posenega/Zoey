0eb7c3105c36bd32de606f791da1a455c5fb531f
Theo Khalil <theokhalil0@icloud.com>
2021-07-22 09:41:22 +0300

    fixes

--- App.js
+++ App.js
@@ -1,52 +1,55 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { useNetInfo } from "@react-native-community/netinfo";
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { Provider } from "react-redux";
-import axios from "axios";
-import MainNavigator from "./navigation/MainNavigator";
-import * as Font from "expo-font";
-import AppLoading from "expo-app-loading";
-import { setCustomText } from "react-native-global-props";
-import booksReducer from "./store/reducers/books";
-import addBookModalReducer from "./store/reducers/addBookModal";
-import AddBookModal from "./components/AddBookModal";
-import thunk from "redux-thunk";
-import authReducer from "./store/reducers/auth";
-import checkTokenExpirationMiddleware from "./store/middlewares/checkTokenExpiration";
-import { Alert } from "react-native";
-import chatsReducer from "./store/reducers/chats";
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  useNetInfo,
+  NetInfoStateType,
+} from '@react-native-community/netinfo';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import MainNavigator from './navigation/MainNavigator';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { setCustomText } from 'react-native-global-props';
+import booksReducer from './store/reducers/books';
+import addBookModalReducer from './store/reducers/addBookModal';
+import AddBookModal from './components/AddBookModal';
+import thunk from 'redux-thunk';
+import authReducer from './store/reducers/auth';
+import checkTokenExpirationMiddleware from './store/middlewares/checkTokenExpiration';
+import { Alert } from 'react-native';
+import chatsReducer from './store/reducers/chats';
 
-axios.defaults.baseURL = "http://192.168.1.114:5000";
+axios.defaults.baseURL = 'https://stormy-garden-51665.herokuapp.com';
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    rubik: require("./assets/fonts/Rubik-Regular.ttf"),
-    "rubik-bold": require("./assets/fonts/Rubik-Bold.ttf"),
-    "rubik-medium": require("./assets/fonts/Rubik-Medium.ttf"),
+    rubik: require('./assets/fonts/Rubik-Regular.ttf'),
+    'rubik-bold': require('./assets/fonts/Rubik-Bold.ttf'),
+    'rubik-medium': require('./assets/fonts/Rubik-Medium.ttf'),
   });
 };
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  const { isConnected } = useNetInfo();
+  // const { isConnected, type } = useNetInfo();
 
-  const showNetworkAlert = useCallback(() => {
-    if (!isConnected) {
-      Alert.alert(
-        "Network error!",
-        "Please check your internet connection and try again later.",
-        [],
-        { cancelable: false }
-      );
-    }
-  }, [isConnected]);
+  // const showNetworkAlert = useCallback(() => {
+  //   if (type !== NetInfoStateType.unknown && !isConnected) {
+  //     Alert.alert(
+  //       'Network error!',
+  //       'Please check your internet connection and try again later.',
+  //       [],
+  //       { cancelable: false }
+  //     );
+  //   }
+  // }, [isConnected, type]);
 
-  useEffect(() => {
-    if (fontsLoaded) {
-      showNetworkAlert();
-    }
-  }, [fontsLoaded, showNetworkAlert]);
+  // useEffect(() => {
+  //   if (fontsLoaded) {
+  //     showNetworkAlert();
+  //   }
+  // }, [fontsLoaded, showNetworkAlert]);
 
   fetchFonts().then(() => setFontsLoaded(true));
 
@@ -54,7 +57,7 @@ export default function App() {
     return <AppLoading />;
   }
 
-  setCustomText({ style: { fontFamily: "rubik" } });
+  setCustomText({ style: { fontFamily: 'rubik' } });
 
   const rootReducer = combineReducers({
     books: booksReducer,
